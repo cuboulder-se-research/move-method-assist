fac3e0fbc6fe66327a384dc98ebb7151b4a3a1aa
Abhiram98 <bellur.abhiram@gmail.com>
2024-08-25 13:03:58 -0600

    fix method signature return type regex

--- src/main/kotlin/com/intellij/ml/llm/template/utils/MethodSignature.kt
+++ src/main/kotlin/com/intellij/ml/llm/template/utils/MethodSignature.kt
@@ -7,11 +7,11 @@ data class Parameter(val name: String, val type: String)
 data class MethodSignature(val methodName: String, val paramsList: List<Parameter>, val returnType:String, val modifier: String){
     companion object{
         fun getMethodSignatureParts(methodSignature: String): MethodSignature? {
-            val methodSignatureRegex = Pattern.compile("(\\w+) (\\w+)(\\(.*\\)) : (\\w+)")
+            val methodSignatureRegex = Pattern.compile("(\\w+) (\\w+)(\\(.*\\)) : ((\\w(<\\w+>)?)+)")
             var m = methodSignatureRegex.matcher(methodSignature)
             if (m.find()) {
 //            val methodInformation = methodSignature.strip().split(" ")
-                val modifier = m.group(1)
+                val modifier = if (m.group(1)=="package") "packageLocal" else m.group(1)
 //            val methodNameAndParams = methodInformation[1]
                 val methodName = m.group(2)
                 val methodParams = m.group(3)
