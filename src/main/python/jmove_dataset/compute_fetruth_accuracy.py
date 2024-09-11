255d5e40f9d09423214d77e15a0b7367f978eefe
Abhiram98 <bellur.abhiram@gmail.com>
2024-09-11 09:58:29 -0600

    add more synthetic data

--- src/main/python/jmove_dataset/compute_fetruth_accuracy.py
+++ src/main/python/jmove_dataset/compute_fetruth_accuracy.py
@@ -1,6 +1,4 @@
 import json
-from collections import Counter, defaultdict
-
 
 def myindex(list, ele):
     try:
@@ -8,53 +6,42 @@ def myindex(list, ele):
     except ValueError:
         return -1
 
-
-def get_jmove_rec(jmove_line):
-    ref, source_method_class, target_class, similarity = jmove_line.split("	")
-    source_class, source_method = source_method_class.split("::")
-    return (ref,
-            source_method.split('(')[0],
-            source_class.split('.')[-1],
-            target_class.split('.')[-1],
-            float(similarity))
-
-
-jmove_out_files = ['jmove_ant_large.txt',
-                   'jmove_ant_small.txt',
-                   'jmove_derby_large.txt',
-                   'jmove_derby_small.txt',
-                   'jmove_drjava_large.txt',
-                   'jmove_drjava_small.txt',
-                   'jmove_jfreechart_large.txt',
-                   'jmove_jfreechart_small.txt',
-                   'jmove_jgroups_large.txt',
-                   'jmove_jgroups_small.txt',
-                   'jmove_jhotdraw_large.txt',
-                   'jmove_jhotdraw_small.txt',
-                   'jmove_jtopen_large.txt',
-                   'jmove_jtopen_small.txt',
-                   'jmove_junit_large.txt',
-                   'jmove_junit_small.txt',
-                   'jmove_lucene_large.txt',
-                   'jmove_lucene_small.txt',
-                   'jmove_mvnforum_large.txt',
-                   'jmove_mvnforum_small.txt',
-                   'jmove_tapestry_large.txt',
-                   'jmove_tapestry_small.txt'
-                   ]
+files = ['ant_large.json',
+         'ant_small.json',
+         'derby_large.json',
+         'derby_small.json',
+         'drjava_large.json',
+         'drjava_small.json',
+         'jfreechart_large.json',
+         'jfreechart_small.json',
+         'jgroups_large.json',
+         'jgroups_small.json',
+         'jhotdraw_large.json',
+         'jhotdraw_small.json',
+         'jtopen_large.json',
+         'jtopen_small.json',
+         'junit_large.json',
+         'junit_small.json',
+         'lucene_large.json',
+         'lucene_small.json',
+         'mvnforum_large.json',
+         'mvnforum_small.json',
+         'tapestry_large.json',
+         'tapestry_small.json']
 
 with open('../../../../data/synthetic_corpus_comparison/oracle/oracle.json') as f:
     oracle_data = json.load(f)
 
 oracle_points = []
 covered = []
-for jmove_file in jmove_out_files:
-    project = jmove_file.split('_')[1]
-    size = jmove_file.split('_')[-1].rstrip('.txt')
+
+for file in files:
+
+    project = file.split('_')[0]
+    size = file.split('_')[-1].rstrip('.json')
     # print(oracle_data[project][size])
-    with open(f'../../../../data/synthetic_corpus_comparison/jmove_run/{jmove_file}') as f:
-        jmove_out = f.read()
-        jmove_recs = [get_jmove_rec(i) for i in jmove_out.split('\n')[1:-1]]
+    with open(f'../../../../data/synthetic_corpus_comparison/feTruth_run/{file}') as f:
+        fetruth_recs = json.load(f)
 
     for oracle in oracle_data[project][size].split('\n'):
         if oracle == '':
@@ -66,39 +53,30 @@ for jmove_file in jmove_out_files:
         alias_method_name = method_name + '2'
         target_class = oracle.split('.')[-1]
         source_class = oracle.split("::")[0].split('.')[-1]
-        # find jmove recomendations for that host class.
+        # find fetruth recomendations for that host class.
 
-        jmove_recs_source_class = [(jsource_method, jtarget_class, similarity)
-                                   for _, jsource_method, jsource_class, jtarget_class, similarity in jmove_recs
-                                   if jsource_class.split('.')[-1] == source_class]
-        print(f"{len(jmove_recs_source_class)=}")
-        if len(jmove_recs_source_class) == 0:
+        fetruth_recs_source_class = [
+            i for i in fetruth_recs if i['source_class'].split('.')[-1] == source_class
+        ]
+        print(f"{len(fetruth_recs_source_class)=}")
+        if len(fetruth_recs_source_class) == 0:
             oracle_result['recall_method_position'] = -1
             oracle_result['recall_method_class_position'] = -1
             continue
 
-        jmove_recs_source_class = sorted(jmove_recs_source_class, key=lambda x: x[-1], reverse=True)
-        jmove_methods = [jmove_method for jmove_method, _, _ in jmove_recs_source_class]
+        fe_methods = [i['source_method'] for i in fetruth_recs_source_class]
         oracle_result['recall_method_position'] = max(
-            myindex(jmove_methods, method_name),
-            myindex(jmove_methods, alias_method_name)
+            myindex(fe_methods, method_name),
+            myindex(fe_methods, alias_method_name)
         )
         if oracle_result['recall_method_position'] == -1:
             oracle_result['recall_method_class_position'] = -1
             continue
         method_index = oracle_result['recall_method_position']
-        jtarget_class = jmove_recs_source_class[method_index][1]
-        if jtarget_class == target_class:
-            oracle_result['recall_method_class_position'] = 0
-        else:
-            oracle_result['recall_method_class_position'] = -1
+        fe_method = fetruth_recs_source_class[method_index]['source_method']
+        fe_target_classes = [i['target_class'].split('.')[-1] for i in fetruth_recs_source_class if i['source_method'] == fe_method]
+        oracle_result['recall_method_class_position'] = myindex(fe_target_classes, target_class)
 
-# covered_vals = Counter(covered)
-# si = defaultdict(lambda :defaultdict(int))
-# for p in oracle_data:
-#     for s in oracle_data[p]:
-#         si[p][s] = len([i for i in oracle_data[p][s].split('\n') if i!=''])
-# assert (si == sum(covered_vals.values()))
 
 recall_method_and_class_1 = len(
     [i for i in oracle_points if i['recall_method_position'] == 0 and i['recall_method_class_position'] == 0]) / len(
