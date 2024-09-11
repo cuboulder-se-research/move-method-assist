6c16d442bf18ff3b3bac7fb238c402ac241bc7d0
Abhiram98 <bellur.abhiram@gmail.com>
2024-09-11 10:30:18 -0600

    fix json sanitise

--- src/main/kotlin/com/intellij/ml/llm/template/utils/JsonUtils.kt
+++ src/main/kotlin/com/intellij/ml/llm/template/utils/JsonUtils.kt
@@ -2,6 +2,18 @@ package com.intellij.ml.llm.template.utils
 
 class JsonUtils {
     companion object{
-        fun sanitizeJson(jsonText: String) = jsonText.removePrefix("```json").removeSuffix("```")
+        fun sanitizeJson(jsonText: String): String {
+            val t1 =  jsonText.removePrefix("```json").removeSuffix("```")
+            if (t1.contains("```json")){
+                return t1.split("```json")[1].split("```")[0]
+            }
+            if (t1.contains("```JSON")){
+                return t1.split("```JSON")[1].split("```")[0]
+            }
+            if (t1.contains("```")){
+                return t1.split("```")[1]
+            }
+            return t1
+        }
     }
 }
